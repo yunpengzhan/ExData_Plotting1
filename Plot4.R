49cb1169d51b41d1ce3e62e75136fcd1bb4e9449
yunpengzhan <38339147+yunpengzhan@users.noreply.github.com>
2018-06-12 21:48:27 -0400

    Create Plot4.R

---
+++ Plot4.R
@@ -0,0 +1,16 @@
+# Plot 4
+# open device
+if(!file.exists('figures')) dir.create('figures')
+png(filename = './figures/plot4.png', width = 480, height = 480, units='px')
+# plot figure
+Sys.setlocale(category = "LC_ALL", locale = "english")
+par(mfrow = c(2, 2))
+plot(data$DateTime, data$Global_active_power, xlab = '', ylab = 'Global Active Power (kilowatt)', type = 'l')
+plot(data$DateTime, data$Voltage, xlab = 'datetime', ylab = 'Voltage', type = 'l')
+plot(data$DateTime, data$Sub_metering_1, xlab = '', ylab = 'Energy sub metering', type = 'l')
+lines(data$DateTime, data$Sub_metering_2, col = 'red')
+lines(data$DateTime, data$Sub_metering_3, col = 'blue')
+legend('topright', col = c('black', 'red', 'blue'), legend = c('Sub_metering_1', 'Sub_metering_2', 'Sub_metering_3'), lwd = 1)
+plot(data$DateTime, data$Global_reactive_power, xlab = 'datetime', ylab = 'Global_reactive_power', type = 'l')
+# close device
+dev.off()
