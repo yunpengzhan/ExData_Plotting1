914453cf6a1969b515be2c3fa3f3f6a7e40782d1
yunpengzhan <38339147+yunpengzhan@users.noreply.github.com>
2018-06-12 21:48:08 -0400

    Create Plot3.R

---
+++ Plot3.R
@@ -0,0 +1,12 @@
+# Plot 3
+# open device
+if(!file.exists('figures')) dir.create('figures')
+png(filename = './figures/plot3.png', width = 480, height = 480, units='px')
+# plot figure
+Sys.setlocale(category = "LC_ALL", locale = "english")
+plot(data$DateTime, data$Sub_metering_1, xlab = '', ylab = 'Energy sub metering', type = 'l')
+lines(data$DateTime, data$Sub_metering_2, col = 'red')
+lines(data$DateTime, data$Sub_metering_3, col = 'blue')
+legend('topright', col = c('black', 'red', 'blue'), legend = c('Sub_metering_1', 'Sub_metering_2', 'Sub_metering_3'), lwd = 1)
+# close device
+dev.off()
